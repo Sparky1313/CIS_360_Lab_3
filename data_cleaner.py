c3c12f0620a187b22a3163ac94d3aebfa23efe65
Trevor Foland <trevorfoland@outlook.com>
2021-04-05 23:52:21 -0400

    Merged several files in data_cleaner.py, cleaned up code, and handled missing values better.

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,28 +1,27 @@
 import requests
 import os.path
-import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# FILE_URL = "https://www.glerl.noaa.gov/res/projects/ifyle/data/Mooring/ysi/2007/Y10.txt" # our actual file
+FILE_URL = "https://www.glerl.noaa.gov/res/projects/ifyle/data/Mooring/ysi/2007/Y07.txt"
+RAW_DATA_FILE = "./data/raw_data_sta_10.txt"
+CLEANED_DATA_FILE = "./data/cleaned_data_sta_10.txt"
 
 def extract():
     #add in a try block
+    req = requests.get(FILE_URL, allow_redirects = True)
 
-    if not os.path.exists("./data/raw_data_sta_10.txt"):
-        url = "https://www.glerl.noaa.gov/res/projects/ifyle/data/Mooring/ysi/2007/Y10.txt"
-        req = requests.get(url, allow_redirects = True)
+    with open(RAW_DATA_FILE, "wb") as extract_file:
+        extract_file.write(req.content)
 
-        with open("./data/raw_data_sta_10.txt", "wb") as extract_file:
-            extract_file.write(req.content)
-        
-    else:
-        print("cool")
 
 def clean():
-    with open("./data/raw_data_sta_10.txt", "r") as data_file, open("./data/cleaned_data_sta_10.txt", "w") as clean_file:
+    with open(RAW_DATA_FILE, "r") as raw_file, open(CLEANED_DATA_FILE, "w") as clean_file:
 
-        # data_file.seek(0)
-        for line in data_file:
+        for line in raw_file:
             fields = line.split()
-
-            # time.sleep(5)
 
             if len(fields) == 0 or fields[0] == "YSI":
                 continue
@@ -30,11 +29,10 @@ def clean():
                 clean_file.write(fields[0] + "\t" + fields[4] + "\n")
             else:
                 clean_file.write(fields[0] + "\t")
-
                 dissolved_oxy = ""
                 
                 if fields[4] == "-999.000":
-                    dissolved_oxy = "Missing"
+                    dissolved_oxy = "nan"
                 elif float(fields[4]) < 0:
                     dissolved_oxy = "0.000"
                 else:
@@ -42,5 +40,42 @@ def clean():
                 
                 clean_file.write(dissolved_oxy + "\n")
 
-extract()
-clean()
+
+def visualize():
+    dates = []
+    oxy_lvls = []
+    # clrs = []
+
+    data_file = open(CLEANED_DATA_FILE, 'r')
+    for row in data_file:
+        row = row.split()
+        
+        if row[0] != "date":
+            dates.append(float(row[0]))
+            oxy_lvls.append(float(row[1]))
+            # if float(row[1]) <= 2:
+            #     clrs.append('r')
+            # else:
+            #     clrs.append('r')
+
+    plt.plot(dates,oxy_lvls, label="Oxygen level throughout year")
+    # plt.locator_params(axis="x", nbins=10)
+    plt.ylim([0, 15])
+    plt.xlim([199, 268])
+    # plt.xticks(np.arange(199, 268, 5), rotation="vertical")
+    plt.axhline(y=2, color='r', linestyle='-', label="Threshold for hypoxia")
+    plt.xlabel('Day of Year (out of 365 days)', fontsize=12)
+    plt.ylabel('Oxygen Levels (mg/L)', fontsize=12)
+
+    plt.title('Oxygen Levels at Station 10 in 2007', fontsize=20)
+    plt.legend()
+    plt.show()
+
+
+if not os.path.exists(RAW_DATA_FILE):
+    extract()
+
+if not os.path.exists(CLEANED_DATA_FILE):
+    clean()
+
+visualize()
